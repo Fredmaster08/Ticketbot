01c6bc1e4de0123559336645cb94c3afcabc2926
Fredmaster08 <frxdmaster@gmail.com>
2025-09-18 07:46:17 +0200

    mouse input

--- ticket.c
+++ ticket.c
@@ -1,9 +1,66 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
 
-int main() {
 
-    printf("Hello.\n");
+#define X 123
+#define Y -123
+#define SCREEN_WIDTH 2560
+#define SCREEN_HEIGHT 1440
+
+
+void MouseSetup(INPUT *buffer)
+{
+    buffer->type = INPUT_MOUSE;
+    buffer->mi.dx = (0 * (0xFFFF / SCREEN_WIDTH));
+    buffer->mi.dy = (0 * (0xFFFF / SCREEN_HEIGHT));
+    buffer->mi.mouseData = 0;
+    buffer->mi.dwFlags = MOUSEEVENTF_ABSOLUTE;
+    buffer->mi.time = 0;
+    buffer->mi.dwExtraInfo = 0;
+}
+
+void MouseMoveRelative(INPUT *buffer, int x, int y) {
+    buffer->mi.dx = x;
+    buffer->mi.dy = y;
+    buffer->mi.dwFlags = MOUSEEVENTF_MOVE;
+    SendInput(1, buffer, sizeof(INPUT));
+}
+
+void MouseMoveAbsolute(INPUT *buffer, int x, int y)
+{
+    buffer->mi.dx = (x * (0xFFFF / SCREEN_WIDTH));
+    buffer->mi.dy = (y * (0xFFFF / SCREEN_HEIGHT));
+    buffer->mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE);
+
+    SendInput(1, buffer, sizeof(INPUT));
+}
+
+
+void MouseClick(INPUT *buffer)
+{
+    buffer->mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN);
+    SendInput(1, buffer, sizeof(INPUT));
+
+    Sleep(10);
+
+    buffer->mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTUP);
+    SendInput(1, buffer, sizeof(INPUT));
+}
+
+
+int main(int argc, char *argv[])
+{
+    INPUT buffer;
+
+    MouseSetup(&buffer);
+
+    MouseMoveRelative(&buffer, -2000, 0);
+    MouseClick(&buffer);
 
     return 0;
 }
